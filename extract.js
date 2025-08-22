8772bb68a3e31a6c00ee4bee33782a0b71bdbce8
Vidar <vidar1812@gmail.com>
2025-08-22 10:45:26 +0200

    Added extract.js and data

---
+++ extract.js
@@ -0,0 +1,44 @@
+// import exifr - a metadata extractor for images
+import exifr from 'exifr';
+// import fs (file system) - a built in module in Node.js
+import fs from 'fs';
+import mysql from 'mysql2/promise';
+import 'dotenv/config';
+
+// Create a connection 'db' to the database
+const db = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'dm24-hbg-grupp5'
+});
+
+// A small function for a query
+async function query(sql){
+   let result = await db.execute(sql);
+   return result[0];
+}
+// let result = await query('SELECT * FROM test')
+// console.log(result)
+
+// give me a list of all files in the image folder
+let images = fs.readdirSync('Data');
+
+
+// Loop through the images and extract the metadata
+for (let image of images) {
+  // let metadata_list = []
+  // Only for files ending with .jpg
+  // slice(-4) get the last 4 letters from the image name
+  if (image.slice(-4) == '.jpg') {
+
+    let raw = await exifr.parse('Data/' + image);
+
+    let metadata = JSON.stringify(raw);
+    // Uploads row by row into database with local storage path as url
+    let [result] = await db.execute('INSERT INTO files (fileName, url, metadata) VALUES (?,?,?)', [image, 'Data/' + image, metadata]);
+    console.log(result)
+  }
+}
+await db.end()
